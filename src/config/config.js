84fc968f1a5a2286fe4ca54a3bef1ce60ec9dfdf
Bhrugu <BT0006@ajanmane.com>
2023-01-28 05:00:09 +0530

    config changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-// export const BaseUrl = "https://idonate.ajanmane.com";
-export const BaseUrl = "https://dfd7-103-85-8-121.in.ngrok.io";
+export const BaseUrl = "https://idonate.ajanmane.com";
+// export const BaseUrl = "https://dfd7-103-85-8-121.in.ngrok.io";
 export const StripeKey =
   "pk_test_51MUhcMDa29JlcCbLq4QzPSL2TVFvVoaVfmri6pTF6UALCzzSmTQEeobb8CaudUpPiY1YyLDafTBTTjwgh4pcoaIs00AhXqbLxE";
