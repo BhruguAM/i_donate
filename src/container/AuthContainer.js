b45460139b5030730f693739742b053df6d0635c
Bhrugu <BT0006@ajanmane.com>
2023-02-01 01:36:37 +0530

    header removed from sign in

--- src/container/AuthContainer.js
+++ src/container/AuthContainer.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "../component";
 import IcAuthCenter from "../assets/icons/ic-auth-center.svg";
 
 const AuthContainer = () => {
+  const location = useLocation();
   return (
     <div className="min-h-screen min-w-full bg-gradient-to-b from-primary via-orange-400 to-primary">
-      <Header />
+      {!location.pathname.includes("/auth/signin") && <Header />}
       <div className="flex flex-col p-4 pt-20">
         <img
           className="absolute w-screen max-w-2xl object-contain left-0 right-0 top-0 bottom-0 m-auto"
